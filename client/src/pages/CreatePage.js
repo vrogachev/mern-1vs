b96042f6c4c6279c9614b2c7419bd822f3440687
Vladislav <r.vlad@1vs.kz>
2020-01-31 16:20:20 +0600

    first commit

---
+++ client/src/pages/CreatePage.js
@@ -0,0 +1,49 @@
+import React, {useContext, useEffect, useState} from 'react';
+import {useHttp} from "../hooks/http.hook";
+import {useHistory} from 'react-router-dom';
+import {AuthContext} from "../context/AuthContext";
+
+export const CreatePage = () => {
+    const history = useHistory();
+    const auth = useContext(AuthContext);
+    const {request} = useHttp();
+    const [link, setLink] = useState('');
+
+    useEffect(() => {
+        window.M.updateTextFields()
+    }, []);
+
+
+    const pressHandler = async event => {
+        console.log(auth.token);
+        if (event.key === 'Enter') {
+            try {
+                const data = await request('/api/link/generate', 'POST', {from: link}, {
+                    Authorization: `Bearer ${auth.token}`
+                });
+                history.push(`/detail/${data.link._id}`);
+                console.log(data);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    };
+
+    return (
+        <div className="row">
+            <div className="col s8 offset-s2" style={{paddingTop: '2rem'}}>
+                <div className="input-field">
+                    <input
+                        placeholder="Вставьте ссылку"
+                        id="link"
+                        type="text"
+                        value={link}
+                        onChange={e => setLink(e.target.value)}
+                        onKeyPress={pressHandler}
+                    />
+                    <label htmlFor="link">Введите ссылку</label>
+                </div>
+            </div>
+        </div>
+    )
+};
